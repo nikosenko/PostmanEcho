8c4c450217a541cbeccd37faeedf1abf9f00f555
Natalia test <n.i.kosenko@bk.ru>
2024-05-06 11:13:03 +0300

    add test

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,26 @@
+package ru.netology.rest;
+
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldPostmanEchoTest() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Helloy!") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Helloy!"))
+        ;
+    }
+}
